cc750347dc2b3a6e82c0ccdcaae3ef4360b5b999
Michael Webster (semiquaver) <semiquaver@mac.com>
2025-05-17 09:18:36 -0700

    from deepseek
    effort to A) show multiple results and B) go to correct line number

--- lua/telescope-supercollider/definitions.lua
+++ lua/telescope-supercollider/definitions.lua
@@ -23,35 +23,63 @@ local scnvim_class_definitions = function(opts)
   local path = require "scnvim/path".get_plugin_root_dir()
   local tagsPath = require "scnvim/path".get_cache_dir() .. "/tags"
   local tagsFile = io.open(tagsPath)
-  local tagPaths = {}
+  local tagEntries = {} -- We'll store both the tag and line number now
 
   for line in tagsFile:lines() do
-    local tagname, tagpath, _, _ = line:match("%s*(.-)\t%s*(.-)\t%s*(.-)\t%s*(.-)")
-    tagPaths[tostring(tagname)] = tagpath
+    local tagname, tagpath, tagline, _ = line:match("%s*(.-)\t%s*(.-)\t%s*(.-)\t%s*(.-)")
+    if tagname and tagpath and tagline then
+      table.insert(tagEntries, {
+        tagname = tagname,
+        tagpath = tagpath,
+        tagline = tonumber(tagline) or 1, -- Default to line 1 if parsing fails
+      })
+    end
   end
 
   tagsFile:close()
 
-  local tagKeys = {}
+  -- Sort the entries alphabetically by tagname
+  table.sort(tagEntries, function(a, b) return a.tagname < b.tagname end)
 
-  for key, _ in pairs(tagPaths) do
-    table.insert(tagKeys, key)
+  -- Create a display table for Telescope
+  local display_entries = {}
+  for _, entry in ipairs(tagEntries) do
+    table.insert(display_entries, {
+      value = entry.tagname,
+      display = entry.tagname,
+      ordinal = entry.tagname,
+      filename = entry.tagpath,
+      lnum = entry.tagline,
+    })
   end
 
-  -- Run a telescope finder that searches through the tagKeys, and when one is chosen, it will open a new file tab with the chosen path
-  require("telescope.pickers").new({}, {
+  -- Run a telescope finder that searches through the tagKeys
+  pickers.new(opts or {}, {
     prompt_title = "SuperCollider class definitions",
-    finder = require("telescope.finders").new_table({
-      results = tagKeys
+    finder = finders.new_table({
+      results = display_entries,
+      entry_maker = function(entry)
+        return {
+          value = entry.value,
+          display = entry.display,
+          ordinal = entry.ordinal,
+          filename = entry.filename,
+          lnum = entry.lnum,
+        }
+      end
     }),
-    sorter = require("telescope.config").values.generic_sorter(opts),
+    sorter = conf.generic_sorter(opts),
+    previewer = previewers.vim_buffer_cat.new(opts), -- Add previewer to see the file content
     attach_mappings = function(prompt_bufnr, map)
       local open_class = function()
-        local selection = require("telescope.actions.state").get_selected_entry()
-        require("telescope.actions").close(prompt_bufnr)
+        local selection = action_state.get_selected_entry()
+        actions.close(prompt_bufnr)
 
-        -- Open the file in a new tab
-        vim.cmd("tabnew " .. tagPaths[selection.value])
+        if selection then
+          -- Open the file in a new tab and jump to the line
+          vim.cmd("tabedit " .. selection.filename)
+          api.nvim_win_set_cursor(0, {selection.lnum, 0})
+        end
       end
 
       map("i", "<CR>", open_class)
@@ -59,7 +87,6 @@ local scnvim_class_definitions = function(opts)
 
       return true
     end,
-
   }):find()
 end
 
