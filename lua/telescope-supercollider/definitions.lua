ff794e2cc1d2a77a1b861cc89b41e4db4bf5d7c8
Michael Webster (semiquaver) <semiquaver@mac.com>
2025-05-17 09:34:24 -0700

    take 2

--- lua/telescope-supercollider/definitions.lua
+++ lua/telescope-supercollider/definitions.lua
@@ -5,43 +5,50 @@ local actions = require 'telescope.actions'
 local action_state = require 'telescope.actions.state'
 local previewers = require 'telescope.previewers'
 local ts_utils = require 'telescope.utils'
-local defaulter = ts_utils.make_default_callable
 
 local uv = vim.loop
 local api = vim.api
 
 local check_scnvim_loaded = function()
-  local scnvim_loaded, scnvim = pcall(require, "scnvim")
-  assert(scnvim_loaded, "SCNvim is not loaded - please make sure to load SCNvim first")
+  local scnvim_loaded = pcall(require, "scnvim")
+  if not scnvim_loaded then
+    vim.notify("SCNvim is not loaded - please make sure to load SCNvim first", vim.log.levels.ERROR)
+    return false
+  end
+  return true
 end
 
 local scnvim_class_definitions = function(opts)
   opts = opts or {}
 
-  check_scnvim_loaded()
+  if not check_scnvim_loaded() then return end
 
-  local path = require "scnvim/path".get_plugin_root_dir()
-  local tagsPath = require "scnvim/path".get_cache_dir() .. "/tags"
-  local tagsFile = io.open(tagsPath)
-  local tagEntries = {} -- We'll store both the tag and line number now
+  local path = require "scnvim.path"
+  local tagsPath = path.get_cache_dir() .. "/tags"
+  
+  local ok, tagsFile = pcall(io.open, tagsPath)
+  if not ok or not tagsFile then
+    vim.notify("Could not open tags file at: " .. tagsPath, vim.log.levels.ERROR)
+    return
+  end
+
+  local tagEntries = {}
 
   for line in tagsFile:lines() do
-    local tagname, tagpath, tagline, _ = line:match("%s*(.-)\t%s*(.-)\t%s*(.-)\t%s*(.-)")
+    local tagname, tagpath, tagline = line:match("^(.-)\t(.-)\t(.-)\t")
     if tagname and tagpath and tagline then
       table.insert(tagEntries, {
         tagname = tagname,
         tagpath = tagpath,
-        tagline = tonumber(tagline) or 1, -- Default to line 1 if parsing fails
+        tagline = tonumber(tagline) or 1,
       })
     end
   end
 
   tagsFile:close()
 
-  -- Sort the entries alphabetically by tagname
   table.sort(tagEntries, function(a, b) return a.tagname < b.tagname end)
 
-  -- Create a display table for Telescope
   local display_entries = {}
   for _, entry in ipairs(tagEntries) do
     table.insert(display_entries, {
@@ -53,9 +60,18 @@ local scnvim_class_definitions = function(opts)
     })
   end
 
-  -- Run a telescope finder that searches through the tagKeys
+  -- Safe file opening function
+  local function safe_open_file(filename, lnum)
+    local ok, _ = pcall(vim.cmd, "silent tabedit " .. vim.fn.fnameescape(filename))
+    if ok then
+      api.nvim_win_set_cursor(0, {lnum, 0})
+    else
+      vim.notify("Failed to open file: " .. filename, vim.log.levels.ERROR)
+    end
+  end
+
   pickers.new(opts or {}, {
-    prompt_title = "SuperCollider class definitions",
+    prompt_title = "SuperCollider Class Definitions",
     finder = finders.new_table({
       results = display_entries,
       entry_maker = function(entry)
@@ -69,16 +85,14 @@ local scnvim_class_definitions = function(opts)
       end
     }),
     sorter = conf.generic_sorter(opts),
-    previewer = previewers.vim_buffer_cat.new(opts), -- Add previewer to see the file content
+    previewer = previewers.vim_buffer_cat.new(opts),
     attach_mappings = function(prompt_bufnr, map)
       local open_class = function()
         local selection = action_state.get_selected_entry()
         actions.close(prompt_bufnr)
 
         if selection then
-          -- Open the file in a new tab and jump to the line
-          vim.cmd("tabedit " .. selection.filename)
-          api.nvim_win_set_cursor(0, {selection.lnum, 0})
+          safe_open_file(selection.filename, selection.lnum)
         end
       end
 
